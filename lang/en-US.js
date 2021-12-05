b0151551675fa3f07b87f5c0fd3177a8db776a61
karlla souzza <karlla.souzza7@gmail.com>
2021-12-05 12:22:34 -0300

    🚀 Added "Sass" and i18n.

---
+++ lang/en-US.js
@@ -0,0 +1,12 @@
+export default {
+  Login: {
+    label: 'Welcome',
+    text: `Lorem ipsum dolor sit amet qweqwe
+            consectetur adipisicing elit.
+            Voluptas asperiores placeat.`,
+    buttons: [
+      { text: 'Login', type: 'button' },
+      { text: 'Create Account', type: 'button' },
+    ],
+  },
+}
--- nuxt.config.js
+++ nuxt.config.js
@@ -9,26 +9,33 @@ export default {
   head: {
     title: 'Chat-APP',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'pt-br',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~static/Scss/color.scss',
+    '~static/Scss/reset.scss',
+    '~static/Scss/mixins.scss',
   ],
 
+  styleResources: {
+    scss: [
+      '~static/Scss/color.scss',
+      '~static/Scss/reset.scss',
+      '~static/Scss/mixins.scss',
+    ],
+  },
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -43,12 +50,41 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/i18n',
   ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  // i18n
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        file: 'en-US.js',
+      },
+      /*  {
+        code: 'es',
+        file: 'es-ES.js',
+      },
+      {
+        code: 'fr',
+        file: 'fr-FR.js',
+      }, */
+    ],
+    lazy: true,
+    langDir: 'lang/',
+    defaultLocale: 'en',
+  },
+
+  proxy: {
+    '/api/': {
+      target: 'https://chat-app-karlla.herokuapp.com/',
+      pathRewrite: { '^/api/': '' },
+    },
+    '/apiDev/': {
+      target: 'https://localhost:4000',
+      pathRewrite: { '^/apiDev/': '' },
+    },
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
