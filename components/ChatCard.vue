f7fd395227831e4b13e6468222890a294e7d4fd1
Karlla Souzza <karlla.souzza7@gmail.com>
2021-12-26 04:31:44 -0300

    Add Fetch, Refesh the Fetch in 0.8s & Render the Messages Cards.

---
+++ components/ChatCard.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="card">
+    <header>
+      <div>
+        <figure>KS</figure>
+        <figcaption>Karlla</figcaption>
+      </div>
+      <div>
+        <MoreCard />
+      </div>
+    </header>
+    <span>
+      {{ text }}
+    </span>
+  </div>
+</template>
+
+<script>
+import MoreCard from '~/components/Svgs/MoreCard.vue'
+export default {
+  components: {
+    MoreCard,
+  },
+  props: {
+    text: {
+      type: String,
+      default: '',
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  width: 90%;
+  height: fit-content;
+  min-height: 80px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+
+  border-radius: 10px;
+  margin: 20px auto;
+  padding: 5px 5px;
+
+  box-shadow: 0 0 4px 1px rgba(0, 0, 0, 0.315);
+
+  header {
+    width: 100%;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    div {
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+
+      &:first-child {
+        width: 40%;
+
+        figure {
+          width: 30px;
+          height: 30px;
+
+          border-radius: 50%;
+
+          background: red;
+
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+        figcaption {
+          font-size: large;
+          font-weight: bold;
+
+          font-family: 'Raleway';
+        }
+      }
+    }
+  }
+
+  span {
+    width: 100%;
+    height: fit-content;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    padding: 0 5px;
+  }
+}
+</style>
---
+++ components/Svgs/MoreCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    width="25"
+    height="25"
+    viewBox="0 0 25 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M6.24365 9.3656L12.4874 15.6094L18.7311 9.3656"
+      stroke="black"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
