ef474c870f7399bffd60a218d7afcd952f69fb3b
Karlla Souzza <karlla.souzza7@gmail.com>
2022-01-02 20:51:27 -0300

    🔥 Add the "socket.io" and methods.

--- pages/index.vue
+++ pages/index.vue
@@ -10,13 +10,16 @@
 </template>
 
 <script>
+import io from 'socket.io-client'
+
 import { mapState } from 'vuex'
+
 export default {
   name: 'Home',
   layout: 'Primary',
   data() {
     return {
-      messages: '',
+      messages: [],
     }
   },
   async fetch() {
@@ -30,13 +33,31 @@ export default {
       user: (state) => state.User.user,
     }),
   },
+
+  mounted() {
+    const socket = io('http://127.0.0.1:4000')
+
+    socket.on('message-created', (serverTask) => {
+      this.messages.push(serverTask)
+    })
+
+    // socket.on('message-updated', (serverTask) => {
+    //   const localTask = this.messages.find(
+    //     (localTask) => localTask.ID === serverTask.ID
+    //   )
+    //   localTask.CONTENT = serverTask.CONTENT
+    // })
+
+    // socket.on('message-removed', (serverTask) => {
+    //   this.messages = this.messages.filter(
+    //     (localTask) => localTask.ID !== serverTask.ID
+    //   )
+    // })
+  },
   created() {
     if (this.user === null) {
       return this.$router.push('/login')
     }
-    setInterval(() => {
-      this.$fetch()
-    }, 800)
   },
 }
 </script>
