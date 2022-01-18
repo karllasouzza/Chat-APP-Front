517f6b3d8b57850413855ab483cb1552928225c6
Karlla Souzza <karlla.souzza7@gmail.com>
2022-01-18 17:44:09 -0300

    ❗️ Create the Blur component.

---
+++ components/BlurPopup.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="popup"></div>
+</template>
+
+<style lang="scss" scoped>
+div#popup {
+  width: 100%;
+  height: 100%;
+
+  background-color: rgba(0, 0, 0, 0.596);
+
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 2;
+
+  cursor: pointer;
+}
+</style>
