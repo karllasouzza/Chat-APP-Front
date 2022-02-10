f25663393d207455b8331f814d3d1afadcd86479
karlla souzza <karlla.souzza7@gmail.com>
2022-02-10 11:30:11 -0300

    🏠 Creating the home screen.

---
+++ components/UserGreet.vue
@@ -0,0 +1,60 @@
+<template>
+  <header class="greet">
+    <span>
+      <Strong>
+        {{ $t('Greet.hello') }} <b>{{ userName }}</b></Strong
+      >
+    </span>
+    <Notifications
+      :active="notification"
+      @click.native="$router.push('/notices')"
+    />
+  </header>
+</template>
+
+<script>
+import Notifications from '~/components/Svgs/Notifications.vue'
+
+export default {
+  components: {
+    Notifications,
+  },
+  data() {
+    return {
+      userName: 'karlla',
+      notification: false,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+header.greet {
+  width: 100%;
+  height: 100%;
+
+  grid-row: 1;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  span {
+    width: 70%;
+    height: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    strong {
+      height: auto;
+      font-size: 1.3rem;
+      font-weight: bold;
+    }
+    b {
+      color: $PrimaryColor;
+    }
+  }
+}
+</style>
---
+++ plugins/vue-toast.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import Toast from 'vue-toastification'
+// Import the CSS or use your own!
+import 'vue-toastification/dist/index.css'
+
+Vue.use(Toast)
